bc1e573cbb4e7de0bc45751ca3d9318e15a1fef3
abhi <knowabhi23@gmail.com>
2017-06-27 16:49:04 +0530

    Adding the Rater Interface

---
+++ src/com/coursera/models/EfficientRater.java
@@ -0,0 +1,55 @@
+//package com.coursera.models;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//
+//public class EfficientRater implements Rater {
+//	private String myID;
+//	private HashMap<String , Rating> myRatings;
+//
+//    public EfficientRater(String id) {
+//        myID = id;
+//        myRatings = new HashMap<String, Rating>();
+//    }
+//
+//    public void addRating(String item, double rating) {
+//        myRatings.add(new Rating(item,rating));
+//    }
+//
+//    public boolean hasRating(String item) {
+//        for(int k=0; k < myRatings.size(); k++){
+//            if (myRatings.get(k).getItem().equals(item)){
+//                return true;
+//            }
+//        }
+//        
+//        return false;
+//    }
+//
+//    public String getID() {
+//        return myID;
+//    }
+//
+//    public double getRating(String item) {
+//        for(int k=0; k < myRatings.size(); k++){
+//            if (myRatings.get(k).getItem().equals(item)){
+//                return myRatings.get(k).getValue();
+//            }
+//        }
+//        
+//        return -1;
+//    }
+//
+//    public int numRatings() {
+//        return myRatings.size();
+//    }
+//
+//    public ArrayList<String> getItemsRated() {
+//        ArrayList<String> list = new ArrayList<String>();
+//        for(int k=0; k < myRatings.size(); k++){
+//            list.add(myRatings.get(k).getItem());
+//        }
+//        
+//        return list;
+//    }
+//}
